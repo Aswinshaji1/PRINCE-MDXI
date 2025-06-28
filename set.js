0fd94f93dcad69819d6d6949af85406c88ede614
Aswinshaji1 <92560292+Aswinshaji1@users.noreply.github.com>
2025-06-28 22:17:56 +0530

    Update set.js

--- set.js
+++ set.js
@@ -10,7 +10,7 @@ global.session = "https://session-id-2fza.onrender.com";
  
  
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PRINCE-MDX~P9hgfCCgpGJVKFlexgxr", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
+SESSION_ID: process.env.SESSION_ID || "PRINCE-MD~H4sIAAAAAAAAA5VU25KiSBD9l3rVGEHuRnTEgojSNCrecWMfSiigAAGrChQn/PcN7Onpedid7eWpyMrIPHnynPoOihJT5KAWjL6DiuAGMtQdWVshMAJGHUWIgD4IIYNgBJxZs99f5z1uZhwC2iyk4THFxbxqrxt2ud0rmodyNb0v+AP3Ah59UNWnHAe/Kai91YcAH4szueGTGKvmQITpxTSztDd1ivwtQMbNi+Etmtkv4NFVhJjgIp5UCTojAnMHtUuIydfgr80W6WifIMVYm+d8aOjXatZA5W06DNBxM0C25vFbiasy8Wvwh/IBTXwVD/jedT6uudxzk9M22h+3Zdg2wa3eDvfc9HVwX3nv8CmOCxTaISoYZu2XeceT2jBkz/avUjLwKkspKRXxHfmO3rPrpQHX1NusrnePD74G3B4LWllQU52WVCJcPKv3lpKHORG0AUzTeFMkWsk1Oy/NfgW+JB9ayf4P73Scns5ZJYtzPjPECQpzzfb2ohDbNT/XuFDBx+iaRWp4yL4G3/I8tRlwN8lUXZhkbbkllid4Z3/vm0tfcEW3mCJuuLVz8RM+ZDX5HcoqKJ3LZjbZFlXlZ+063m1MjXDNtDV3FqUkuqTSekz1JHHXWQBdYX5Je2+8bXozRSnnUqRqccNl+UnX/TnnapGXKab38pwoQ60dghH/6AOCYkwZgQyXRRcTh30Aw2aNAoLYk12wvDInOgvYyqVks4iXe2GzdZBZ3zXmavfJ0RCDsbzZLJK7+gL6oCJlgChF4QxTVpLWRZTCGFEw+vOvPijQjb3vresm8H0QYULZtqirvIThx1I/LmEQlHXB1m0RjLsDImDEfYYRY7iIaUdjXUASJLhB4wQyCkYRzCn6OSAiKAQjRmr007TjMux4P+6G7lqWPdAH5+c+cNi9BLwmCRqvDnlRHWn8H/TbtSsLq+pbgRjog/yZpqqKKPC8Igu8JEtdYhd//ATY1QsRgzinYATGC5MOmG1N5o4Qu+50qk9ifRzr4HOgD2G8M58lsqO87iRzJ/fE/bpeS0kit4dZc5gI0i3l4+uuyevZPWknL/9QBIwAdzl591Utsy1lQXZu9KU4NaeJcz9ahqrsYks1/Jzuh4p13vaqrcYOp7uavI21+d5qZqvXVZ4mBydVOF4aizd1s7qEnnF96bqFqMEB+rVZps4rejNahzcWlp9aG9O+W7K1Wl0CLK3b8nLdLLV7QPLqojnK0FiZNTmf0tdSmr8mqyyQRI1vpMUp5rOLqyvqnp0MU3+X7NMy+Y+nCj/V1K2q+40wejq/gN0C/3t178A7hXGP/i81frwl/+JH42gjXLc75PGoqa2MZLXvI5rfFP427wW+5rvWSTolqhgk4PH4qw+qHLKoJGcwArAISYlD0AekrDvJ2kVU/qbZWN/aH2bNIWX6pw02+Iwog+cKjHhF4vmhxkn8e9aSlNUM0gSMgBD517HTabrVq2rNIPtwFdC7zx0y8PgbJ/dBUnAHAAA=", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
 SUDO_NUMBERS: process.env.SUDO_NUMBERS || "", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
 ANTI_DELETE: process.env.ANTI_DELETE || "inboxonly", // can be set to inboxonly/allchats/true/false
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
@@ -20,8 +20,8 @@ AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
 STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by prince-mdx", // // Input Yours custom...
 MODE: process.env.MODE || "public", // Put private or public or inbox or groups
 OWNER_NUMBER: process.env.OWNER_NUMBER || "917736321747", // Only 1 owner Number Here, others Add to sudo numbers...
-OWNER_NAME: process.env.OWNER_NAME || "𝑅𝛩𝑌𝛥𝐿 𝑆𝛯𝑅", // Input Yours custom...(Maintain font for Flow)
-PACK_AUTHOR: process.env.PACK_AUTHOR || "𝐏𝐑𝐈𝐍𝐂𝐄 𝐓𝐄𝐂𝐇", // Added // Input Yours custom...
+OWNER_NAME: process.env.OWNER_NAME || "᥊ꫀꪀꪮꪑꪮ𝘳ρꫝ🍃🤍", // Input Yours custom...(Maintain font for Flow)
+PACK_AUTHOR: process.env.PACK_AUTHOR || "𝐱𝐞𝐧𝐨𝐦𝐨𝐫𝐩𝐡", // Added // Input Yours custom...
 PACK_NAME: process.env.PACK_NAME || "X-BOT", // Added // Input Yours custom...
 PREFIX: process.env.PREFIX || ".",
 VERSION: process.env.VERSION || "3.0.0",
@@ -32,7 +32,7 @@ BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Add Yours Sep
 ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 📵 No Calls Allowed!_*",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 BOT_NAME: process.env.BOT_NAME || "X-BOT", //  don't change 
-BOT_PIC: process.env.BOT_PIC || "https://raw.githubusercontent.com/Mayelprince/url/main/menun.jpg", //  don't change 
+BOT_PIC: process.env.BOT_PIC || "https://cdn.vioo.my.id/v/HTppsn.png", //  don't change 
 AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
 AUTO_BIO: process.env.AUTO_BIO || "false",
 AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || " powered by prince",
